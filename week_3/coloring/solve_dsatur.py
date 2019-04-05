8bdbd585a8849e1534ee858b9085c8553e6be0fe
lazydancer <jrpucula@gmail.com>
2019-04-04 22:12:45 -0400

    Before updating dstar with numba

--- week_3/coloring/solve_dsatur.py
+++ week_3/coloring/solve_dsatur.py
@@ -1,15 +1,11 @@
 import networkx as nx
 import itertools
+import random
+
+from tqdm import tqdm
 
 
 def strategy_saturation_largest_first(G, colors):
-    """Iterates over all the nodes of ``G`` in "saturation order" (also
-    known as "DSATUR").
-
-    ``G`` is a NetworkX graph. ``colors`` is a dictionary mapping nodes of
-    ``G`` to colors, for those nodes that have already been colored.
-
-    """
     distinct_colors = {v: set() for v in G}
     for i in range(len(G)):
         # On the first time through, simply choose the node of highest degree.
@@ -34,109 +30,31 @@ def strategy_saturation_largest_first(G, colors):
             for v in G[node]:
                 distinct_colors[v].add(color)
 
-
-STRATEGIES = {
-    'saturation_largest_first': strategy_saturation_largest_first,
-    'DSATUR': strategy_saturation_largest_first,
-}
-
-def greedy_color(G, strategy='largest_first', interchange=False):
-    """Color a graph using various strategies of greedy graph coloring.
-
-    Attempts to color a graph using as few colors as possible, where no
-    neighbours of a node can have same color as the node itself. The
-    given strategy determines the order in which nodes are colored.
-
-    The strategies are described in [1]_, and smallest-last is based on
-    [2]_.
-
-    Parameters
-    ----------
-    G : NetworkX graph
-
-    strategy : string or function(G, colors)
-       A function (or a string representing a function) that provides
-       the coloring strategy, by returning nodes in the ordering they
-       should be colored. ``G`` is the graph, and ``colors`` is a
-       dictionary of the currently assigned colors, keyed by nodes. The
-       function must return an iterable over all the nodes in ``G``.
-
-       If the strategy function is an iterator generator (that is, a
-       function with ``yield`` statements), keep in mind that the
-       ``colors`` dictionary will be updated after each ``yield``, since
-       this function chooses colors greedily.
-
-       If ``strategy`` is a string, it must be one of the following,
-       each of which represents one of the built-in strategy functions.
-
-       * ``'largest_first'``
-       * ``'random_sequential'``
-       * ``'smallest_last'``
-       * ``'independent_set'``
-       * ``'connected_sequential_bfs'``
-       * ``'connected_sequential_dfs'``
-       * ``'connected_sequential'`` (alias for the previous strategy)
-       * ``'strategy_saturation_largest_first'``
-       * ``'DSATUR'`` (alias for the previous strategy)
-
-    interchange: bool
-       Will use the color interchange algorithm described by [3]_ if set
-       to ``True``.
-
-       Note that ``strategy_saturation_largest_first`` and
-       ``strategy_independent_set`` do not work with
-       interchange. Furthermore, if you use interchange with your own
-       strategy function, you cannot rely on the values in the
-       ``colors`` argument.
-
-    Returns
-    -------
-    A dictionary with keys representing nodes and values representing
-    corresponding coloring.
-
-    Examples
-    --------
-    >>> G = nx.cycle_graph(4)
-    >>> d = nx.coloring.greedy_color(G, strategy='largest_first')
-    >>> d in [{0: 0, 1: 1, 2: 0, 3: 1}, {0: 1, 1: 0, 2: 1, 3: 0}]
-    True
-
-    Raises
-    ------
-    NetworkXPointlessConcept
-        If ``strategy`` is ``strategy_saturation_largest_first`` or
-        ``strategy_independent_set`` and ``interchange`` is ``True``.
-
-    References
-    ----------
-    .. [1] Adrian Kosowski, and Krzysztof Manuszewski,
-       Classical Coloring of Graphs, Graph Colorings, 2-19, 2004.
-       ISBN 0-8218-3458-4.
-    .. [2] David W. Matula, and Leland L. Beck, "Smallest-last
-       ordering and clustering and graph coloring algorithms." *J. ACM* 30,
-       3 (July 1983), 417–427. <https://doi.org/10.1145/2402.322385>
-    .. [3] Maciej M. Sysło, Marsingh Deo, Janusz S. Kowalik,
-       Discrete Optimization Algorithms with Pascal Programs, 415-424, 1983.
-       ISBN 0-486-45353-7.
-
-    """
+def greedy_color(G, unique_colors):
     if len(G) == 0:
         return {}
-    # Determine the strategy provided by the caller.
-    strategy = STRATEGIES.get(strategy, strategy)
-    if not callable(strategy):
-        raise nx.NetworkXError('strategy must be callable or a valid string. '
-                               '{0} not valid.'.format(strategy))
     colors = {}
-    nodes = strategy(G, colors)
+    nodes = strategy_saturation_largest_first(G, colors)
     for u in nodes:
         # Set to keep track of colors of neighbours
         neighbour_colors = {colors[v] for v in G[u] if v in colors}
         # Find the first unused color.
-        for color in itertools.count():
+        #for color in itertools.count():
+        #    if color not in neighbour_colors:
+        #        break
+        # Assign the new color to the current node.
+
+        # Find the first random unused color in unique colors.
+        random_colors = random.sample(range(unique_colors), unique_colors)
+
+        for i, color in enumerate(random_colors):
+            if i == unique_colors - 1:
+                # There isn't a solution for this chose
+                return None
+                
             if color not in neighbour_colors:
                 break
-        # Assign the new color to the current node.
+
         colors[u] = color
     return colors
 
@@ -144,17 +62,35 @@ def greedy_color(G, strategy='largest_first', interchange=False):
 def solve(node_count, edge_count, edges):
     G = nx.Graph()
     G.add_edges_from(edges)
-    d = greedy_color(G, strategy='DSATUR')
+
+    if node_count == 50:
+        unique_colors = 6
+    if node_count == 70:
+        unique_colors = 20
+    if node_count == 100:
+        unique_colors = 16
+    if node_count == 250:
+        unique_colors = 78
+    if node_count == 500:
+        unique_colors = 16
+    if node_count == 1000:
+        unique_colors = 100
+    
+    for i in tqdm(range(10000)):
+        result = greedy_color(G, unique_colors)
+        if result:
+            print("After ", i)
+            break
 
     solution = []
     for i in range(node_count):
-        solution.append(d[i])
+        solution.append(result[i])
 
     return(solution,0)
 
-node_count = 4
-edge_count = 4
-edges = [(0,1),(0,2),(0,3),(2,3)]
+#node_count = 4
+#edge_count = 4
+#edges = [(0,1),(0,2),(0,3),(2,3)]
 
-print(solve(node_count, edge_count, edges))
+#print(solve(node_count, edge_count, edges))
 
